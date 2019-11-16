6d441b1912f53cef73fba9355b07f77da276c16c
eleven <599030042@qq.com>
2019-11-16 13:39:17 +0800

    1

--- src/utils/Tmap.js
+++ src/utils/Tmap.js
@@ -23,30 +23,95 @@ TTuJing.prototype.drawRange = function(item, color) {
   this.GetVectorLayer().AddFeatures([pfMansion])
   pfMansion.Activate()
   // 点击事件
-  pfMansion.AddEventListener('click', pfMansion, function() {
-    let position = item.range.split(',')[0].split(' ')
-    var infoText = `<div class="tmapWindow">
-                      <p>户主姓名: ${item.name}</p>
-                      <p>联系人电话: ${item.dimTourBasResidentInfo.phone}</p>
-                      <p>家庭地址: ${item.address}</p>
-                      <p>家庭人数: ${item.dimTourBasResidentInfo.memberNum}</p>
-                      <p>家庭: ${item.dimTourBasResidentInfo.houseTip}</p>
-                    </div>`
-    var lonlat = new SLonLat(position[0], position[1])
-    // TMapAPI.GetMap().SInfoWindow.SetLonLat(lonlat, null, false)
-    TMapAPI.GetMap().SInfoWindow.SetLonLat(lonlat, null, false)
-    // 信息面板显示内容
-    TMapAPI.GetMap().SInfoWindow.SetInnerHTML(infoText)
-    // 面板底部对应图标位置
-    TMapAPI.GetMap().SInfoWindow.SetOffset(new SSize(0, 0))
-    // 信息面板长宽
-    TMapAPI.GetMap().SInfoWindow.SetSize(new SSize(250, 200))
-    TMapAPI.GetMap().SInfoWindow.Show()
-    // 当前点居中,3是层级（0-3层）
-    TMapAPI.GetMap().SetCenter(lonlat, 2)
-    // TMapAPI.GetMap().SInfoWindow2.ReWriteInfoClose(function() {
-    //   alert(1)
-    //   TMapAPI.GetMap().SInfoWindow2.Hide()
-    // })
+  // pfMansion.AddEventListener('click', pfMansion, function() {
+  //   let position = item.range.split(',')[0].split(' ')
+  //   var infoText = `<div class="tmapWindow">
+  //                     <p>户主姓名: ${item.name}</p>
+  //                     <p>联系人电话: ${item.dimTourBasResidentInfo.phone}</p>
+  //                     <p>家庭地址: ${item.address}</p>
+  //                     <p>家庭人数: ${item.dimTourBasResidentInfo.memberNum}</p>
+  //                     <p>家庭: ${item.dimTourBasResidentInfo.houseTip}</p>
+  //                   </div>`
+  //   var lonlat = new SLonLat(position[0], position[1])
+  //   // TMapAPI.GetMap().SInfoWindow.SetLonLat(lonlat, null, false)
+  //   TMapAPI.GetMap().SInfoWindow.SetLonLat(lonlat, null, false)
+  //   // 信息面板显示内容
+  //   TMapAPI.GetMap().SInfoWindow.SetInnerHTML(infoText)
+  //   // 面板底部对应图标位置
+  //   TMapAPI.GetMap().SInfoWindow.SetOffset(new SSize(0, 0))
+  //   // 信息面板长宽
+  //   TMapAPI.GetMap().SInfoWindow.SetSize(new SSize(250, 200))
+  //   TMapAPI.GetMap().SInfoWindow.Show()
+  //   // 当前点居中,3是层级（0-3层）
+  //   TMapAPI.GetMap().SetCenter(lonlat, 2)
+  //   // TMapAPI.GetMap().SInfoWindow2.ReWriteInfoClose(function() {
+  //   //   alert(1)
+  //   //   TMapAPI.GetMap().SInfoWindow2.Hide()
+  //   // })
+  // })
+
+  pfMansion.AddEventListener('mousemove', pfMansion, function() {
+    TMapAPI.map.ShowLabelsByTag('house' + item.id)
   })
+  pfMansion.AddEventListener('mouseout', pfMansion, function() {
+    TMapAPI.map.HideLabelsByTag('house' + item.id)
+  })
+}
+// 绘制标签lable
+TTuJing.prototype.drawRangeLableDefault = function(item) {
+  var infoText =
+    `<div class="tmapWindowDefault">
+                    <p>` +
+    item.name +
+    `</p>
+                  </div>`
+  var sLonLat = new SLonLat(item.lon, item.lat)
+  var lbl = new SLabel(null, sLonLat, null, infoText)
+  lbl.SetTag('default' + item.id)
+  lbl.SetOpacity(0.8)
+  lbl.SetOffset(new SSize(15, -40))
+  lbl.SetAdaptive()
+  TMapAPI.map.AddLabel(lbl)
+}
+// 绘制标签lable
+TTuJing.prototype.drawRangeLableFire = function(item) {
+  var infoText =
+    `<div class="tmapWindowFire">
+                    <p>姓名：` +
+    item.name +
+    `</p>
+                    <p>地址：` +
+    item.address +
+    `</p>
+                    <p>电话：` +
+    item.tel +
+    `</p>
+                  </div>`
+  var sLonLat = new SLonLat(item.lon, item.lat)
+  var lbl = new SLabel(null, sLonLat, null, infoText)
+  lbl.SetTag('fire' + item.id)
+  lbl.SetOpacity(0.8)
+  lbl.SetOffset(new SSize(15, -60))
+  lbl.SetAdaptive()
+  TMapAPI.map.AddLabel(lbl)
+}
+// 绘制标签lable
+TTuJing.prototype.drawRangeLable_house = function(item) {
+  if (!item.range) {
+    return
+  }
+  var sLonLat = new SLonLat(item.centerX, item.centerY)
+  var infoText = `<div class="tmapWindow">
+                    <p>户主姓名: ${item.name}</p>
+                    <p>联系人电话: ${item.dimTourBasResidentInfo.phone}</p>
+                    <p>家庭地址: ${item.address}</p>
+                    <p>家庭人数: ${item.dimTourBasResidentInfo.memberNum}</p>
+                    <p>美丽家庭: ${item.dimTourBasResidentInfo.houseTipName}</p>
+                  </div>`
+  var lbl = new SLabel(null, sLonLat, null, infoText)
+  lbl.SetTag('house' + item.id)
+  lbl.SetOpacity(0.8)
+  lbl.SetOffset(new SSize(0, 0))
+  lbl.SetAdaptive()
+  TMapAPI.map.AddLabel(lbl)
 }
